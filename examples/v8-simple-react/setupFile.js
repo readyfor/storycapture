a77b6fdfdd5a33da2c2d9eb6a97cddb2fc166457
huuyafwww <huuya1234fwww@gmail.com>
2025-07-25 20:57:54 +0900

    Removed pre-v9 examples

--- examples/v8-simple-react/setupFile.js
+++
@@ -1,4 +0,0 @@
-import { setProjectAnnotations } from '@storybook/react';
-import * as globalStorybookConfig from './.storybook/preview';
-
-setProjectAnnotations(globalStorybookConfig);
--- examples/v8-vite5-prod/setupFile.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'
-import { setProjectAnnotations } from '@storybook/react';
-import * as globalStorybookConfig from './.storybook/preview.js';
-
-setProjectAnnotations(globalStorybookConfig);
